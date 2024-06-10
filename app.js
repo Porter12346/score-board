8df5f20f4f9a5f3aec1f361f7f09bf1462ce1096
Porter Young <porteryoung2005@gmail.com>
2024-06-10 14:10:14 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let home = 0
+let away = 0
+function home1() {
+    home++
+    updateLabels();
+}
+
+function away1() {
+    away++
+    updateLabels();
+}
+
+function home3() {
+    home += 3
+    updateLabels();
+}
+
+function away3() {
+    away += 3
+    updateLabels();
+}
+
+function updateLabels() {
+    const homeScoreElement = document.getElementById('home-score')
+    homeScoreElement.innerText = String(home)
+    const awayScoreElement = document.getElementById('away-score')
+    awayScoreElement.innerText = String(away)
+}
